7fb1c7bb83ab1b303305a84bb56f00475e15b517
kimc05 <christinekimsooa@gmail.com>
2022-10-20 01:24:36 -0700

    lesson 8 assignment final submission

--- blogging/views.py
+++ blogging/views.py
@@ -5,31 +5,28 @@ from blogging.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
-'''
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    context = {'posts': posts}
-    return render(request, 'blogging/list.html', context)
-
-
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
-'''
-
 
 class PostListView(ListView):
     model = Post
     template_name = 'blogging/list.html'
+
+    def get(self, request):
+        published = Post.objects.exclude(published_date__exact=None)
+        posts = published.order_by('-published_date')
+        context = {'post_list': posts}
+        return render(request, 'blogging/list.html', context)
+
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blogging/detail.html'
 
+    def detail_view(self, request, post_id):
+        published = Post.objects.exclude(published_date__exact=None)
+        try:
+            post = published.get(pk=post_id)
+        except Post.DoesNotExist:
+            raise Http404
+        context = {'post': post}
+        return render(request, 'blogging/detail.html', context)
